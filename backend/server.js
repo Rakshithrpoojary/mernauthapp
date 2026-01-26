48b8116183c0d7461f2bf978d00352bce808ae64
Rakshith <poojaryrakshith986@gmail.com>
2026-01-26 16:31:49 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 import connectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
@@ -12,7 +13,7 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
