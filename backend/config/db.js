ff7a966bd960549260093afb66c756b06cc3d882
Rakshith <poojaryrakshith986@gmail.com>
2026-01-28 16:47:40 +0530

    jenkins file for frontend

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,12 +3,12 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     // Use MONGO_URI from environment variables
-    console.log("URL",process.env.MONGO_URI);
-    const conn = await mongoose.connect("mongodb://admin:admin123@mongodb-svc:27017/admin");
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
-  setTimeout(connectDB,5000)
+    process.exit(1); // Exit process with failure
   }
 };
 
