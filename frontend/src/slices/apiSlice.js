e3ab96e4e7466adcee9a1ed0997dad0263969d36
Rakshith <poojaryrakshith986@gmail.com>
2026-01-28 16:55:52 +0530

    update deployment.yaml backend

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -5,7 +5,7 @@ import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://backend:80",
+    baseUrl: "http://a6d35c9945ce542ad962db7d36ba636d-1979400558.ap-south-1.elb.amazonaws.com:80",
     credentials: "include",
   }),
   tagTypes: ["User"],
