ae1ebdc9755e408a5d54c7caf4c1686c2978ee35
Rakshith <poojaryrakshith986@gmail.com>
2026-01-30 15:54:44 +0530

    s

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
 dotenv.config();
 import connectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
